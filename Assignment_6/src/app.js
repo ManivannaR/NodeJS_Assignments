6b3231f8373aad08bf665880a51b145284d7c819
ManivannaR <mails4mani123@gmail.com>
2022-11-17 18:50:35 +0530

    Assignment 6 has been added

---
+++ Assignment_6/src/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const { MongoClient } = require("mongodb");
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Import routes
+const blogRoute = require("./routes/blog");
+
+//Router MIddlewares
+app.use(express.json());
+app.use("/", blogRoute);
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "myProject";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+
+  // the following code examples can be pasted here...
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
+module.exports = app;
